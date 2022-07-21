0b20c672d7fd1a464c9cdd839506ca6b6dc51fcf
autotrof <upload.kurniawan@gmail.com>
2022-07-21 07:40:49 +0700

    alert bordered

--- pages/component.vue
+++ pages/component.vue
@@ -295,27 +295,27 @@
         <p class="description"> Thank you for joining our newsletter </p>
       </div>
 
-      <div class="alert primary outline mb-2">
+      <div class="alert primary outlined mb-2">
         <strong class="title">Primary alert</strong>
         <p class="description"> Thank you for joining our newsletter </p>
       </div>
-      <div class="alert secondary outline mb-2">
+      <div class="alert secondary outlined mb-2">
         <strong class="title">Secondary alert</strong>
         <p class="description"> Thank you for joining our newsletter </p>
       </div>
-      <div class="alert warning outline mb-2">
+      <div class="alert warning outlined mb-2">
         <strong class="title">Warning alert</strong>
         <p class="description"> Thank you for joining our newsletter </p>
       </div>
-      <div class="alert danger outline mb-2">
+      <div class="alert danger outlined mb-2">
         <strong class="title">Danger alert</strong>
         <p class="description"> Thank you for joining our newsletter </p>
       </div>
-      <div class="alert success outline mb-2">
+      <div class="alert success outlined mb-2">
         <strong class="title">Success alert</strong>
         <p class="description"> Thank you for joining our newsletter </p>
       </div>
-      <div class="alert info outline mb-2">
+      <div class="alert info outlined mb-2">
         <strong class="title">Info alert</strong>
         <p class="description"> Thank you for joining our newsletter </p>
       </div>
@@ -341,6 +341,32 @@
         <p class="description"> Thank you for joining our newsletter </p>
       </div>
       <div class="alert info mb-2">
+        <strong class="title">Info alert</strong>
+        <p class="description"> Thank you for joining our newsletter </p>
+      </div>
+
+
+      <div class="alert primary bordered mb-2">
+        <strong class="title">Primary alert</strong>
+        <p class="description"> Thank you for joining our newsletter </p>
+      </div>
+      <div class="alert secondary bordered mb-2">
+        <strong class="title">Secondary alert</strong>
+        <p class="description"> Thank you for joining our newsletter </p>
+      </div>
+      <div class="alert warning bordered mb-2">
+        <strong class="title">Warning alert</strong>
+        <p class="description"> Thank you for joining our newsletter </p>
+      </div>
+      <div class="alert danger bordered mb-2">
+        <strong class="title">Danger alert</strong>
+        <p class="description"> Thank you for joining our newsletter </p>
+      </div>
+      <div class="alert success bordered mb-2">
+        <strong class="title">Success alert</strong>
+        <p class="description"> Thank you for joining our newsletter </p>
+      </div>
+      <div class="alert info bordered mb-2">
         <strong class="title">Info alert</strong>
         <p class="description"> Thank you for joining our newsletter </p>
       </div>
