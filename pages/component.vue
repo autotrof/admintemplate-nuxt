69c1fc991c4c7b16c9d2de5a28c1e8ec2a539886
autotrof <upload.kurniawan@gmail.com>
2022-07-24 14:17:48 +0700

    alert icon OK

--- pages/component.vue
+++ pages/component.vue
@@ -3,7 +3,9 @@
     <div class="w-ful mb-2">
       <h2>Widget</h2>
     </div>
-    <div><h5>INFO BOX 1</h5></div>
+    <div>
+      <h5>INFO BOX 1</h5>
+    </div>
     <div class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-4 mb-2">
       <div class="info-box1 primary">
         <div class="icon-wrapper" v-html="$feather['message-square'].toSvg()"></div>
@@ -48,7 +50,7 @@
         </div>
       </div>
       <div class="info-box1 info">
-        <LoadingOverlay/>
+        <LoadingOverlay />
         <div class="icon-wrapper" v-html="$feather['thumbs-up'].toSvg()"></div>
         <div class="info">
           <div>Likes</div>
@@ -57,14 +59,16 @@
       </div>
     </div>
 
-    <div><h5>INFO BOX 2</h5></div>
+    <div>
+      <h5>INFO BOX 2</h5>
+    </div>
     <div class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-4 mb-2">
       <div class="info-box2 primary">
         <div class="icon-wrapper" v-html="$feather['message-square'].toSvg({width:33,height:33})"></div>
         <div class="info">
           <div>Messages</div>
           <strong class="text-sm">1,430</strong>
-          <Progress percentage="70" bgClass="mt-2 mb-1 bg-black bg-opacity-20"/>
+          <Progress percentage="70" bgClass="mt-2 mb-1 bg-black bg-opacity-20" />
           <small>70% increase in 30 days</small>
         </div>
       </div>
@@ -73,7 +77,7 @@
         <div class="info">
           <div>Alert</div>
           <strong class="text-sm">1,430</strong>
-          <Progress percentage="70" bgClass="mt-2 mb-1 bg-black bg-opacity-20"/>
+          <Progress percentage="70" bgClass="mt-2 mb-1 bg-black bg-opacity-20" />
           <small>70% increase in 30 days</small>
         </div>
       </div>
@@ -82,7 +86,7 @@
         <div class="info">
           <div>Bookmark</div>
           <strong class="text-sm">10</strong>
-          <Progress percentage="70" bgClass="mt-2 mb-1 bg-white bg-opacity-30" fgClass="bg-slate-700"/>
+          <Progress percentage="70" bgClass="mt-2 mb-1 bg-white bg-opacity-30" fgClass="bg-slate-700" />
           <small>70% increase in 30 days</small>
         </div>
       </div>
@@ -91,7 +95,7 @@
         <div class="info">
           <div>Likes</div>
           <strong class="text-sm">1,213</strong>
-          <Progress percentage="70" bgClass="mt-2 mb-1 bg-black bg-opacity-20"/>
+          <Progress percentage="70" bgClass="mt-2 mb-1 bg-black bg-opacity-20" />
           <small>70% increase in 30 days</small>
         </div>
       </div>
@@ -100,7 +104,7 @@
         <div class="info">
           <div>Likes</div>
           <strong class="text-sm">1,213</strong>
-          <Progress percentage="70" bgClass="mt-2 mb-1 bg-black bg-opacity-20"/>
+          <Progress percentage="70" bgClass="mt-2 mb-1 bg-black bg-opacity-20" />
           <small>70% increase in 30 days</small>
         </div>
       </div>
@@ -109,30 +113,32 @@
         <div class="info">
           <div>Likes</div>
           <strong class="text-sm">1,213</strong>
-          <Progress percentage="70" bgClass="mt-2 mb-1 bg-white bg-opacity-30" fgClass="bg-slate-700"/>
+          <Progress percentage="70" bgClass="mt-2 mb-1 bg-white bg-opacity-30" fgClass="bg-slate-700" />
           <small>70% increase in 30 days</small>
         </div>
       </div>
       <div class="info-box2 info">
-        <LoadingOverlay/>
+        <LoadingOverlay />
         <div class="icon-wrapper" v-html="$feather['thumbs-up'].toSvg({width:33,height:33})"></div>
         <div class="info">
           <div>Likes</div>
           <strong class="text-sm">1,213</strong>
-          <Progress percentage="70" bgClass="mt-2 mb-1 bg-white bg-opacity-30" fgClass="bg-slate-700"/>
+          <Progress percentage="70" bgClass="mt-2 mb-1 bg-white bg-opacity-30" fgClass="bg-slate-700" />
           <small>70% increase in 30 days</small>
         </div>
       </div>
     </div>
 
-    <div><h5>INFO BOX 2 GRADIENT</h5></div>
+    <div>
+      <h5>INFO BOX 2 GRADIENT</h5>
+    </div>
     <div class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-4 mb-2">
       <div class="info-box2 gradient primary">
         <div class="icon-wrapper" v-html="$feather['message-square'].toSvg({width:33,height:33})"></div>
         <div class="info text-white">
           <div>Messages</div>
           <strong class="text-sm">1,430</strong>
-          <Progress percentage="70" bgClass="mt-2 mb-1 bg-black bg-opacity-20"/>
+          <Progress percentage="70" bgClass="mt-2 mb-1 bg-black bg-opacity-20" />
           <small>70% increase in 30 days</small>
         </div>
       </div>
@@ -141,7 +147,7 @@
         <div class="info">
           <div>Alert</div>
           <strong class="text-sm">1,430</strong>
-          <Progress percentage="70" bgClass="mt-2 mb-1 bg-black bg-opacity-20"/>
+          <Progress percentage="70" bgClass="mt-2 mb-1 bg-black bg-opacity-20" />
           <small>70% increase in 30 days</small>
         </div>
       </div>
@@ -150,7 +156,7 @@
         <div class="info">
           <div>Bookmark</div>
           <strong class="text-sm">10</strong>
-          <Progress percentage="70" bgClass="mt-2 mb-1 bg-white bg-opacity-30" fgClass="bg-slate-700"/>
+          <Progress percentage="70" bgClass="mt-2 mb-1 bg-white bg-opacity-30" fgClass="bg-slate-700" />
           <small>70% increase in 30 days</small>
         </div>
       </div>
@@ -159,7 +165,7 @@
         <div class="info">
           <div>Likes</div>
           <strong class="text-sm">1,213</strong>
-          <Progress percentage="70" bgClass="mt-2 mb-1 bg-black bg-opacity-20"/>
+          <Progress percentage="70" bgClass="mt-2 mb-1 bg-black bg-opacity-20" />
           <small>70% increase in 30 days</small>
         </div>
       </div>
@@ -168,7 +174,7 @@
         <div class="info">
           <div>Likes</div>
           <strong class="text-sm">1,213</strong>
-          <Progress percentage="70" bgClass="mt-2 mb-1 bg-black bg-opacity-20"/>
+          <Progress percentage="70" bgClass="mt-2 mb-1 bg-black bg-opacity-20" />
           <small>70% increase in 30 days</small>
         </div>
       </div>
@@ -177,23 +183,25 @@
         <div class="info">
           <div>Likes</div>
           <strong class="text-sm">1,213</strong>
-          <Progress percentage="70" bgClass="mt-2 mb-1 bg-white bg-opacity-20" fgClass="bg-slate-700"/>
+          <Progress percentage="70" bgClass="mt-2 mb-1 bg-white bg-opacity-20" fgClass="bg-slate-700" />
           <small>70% increase in 30 days</small>
         </div>
       </div>
       <div class="info-box2 gradient info">
-        <LoadingOverlay/>
+        <LoadingOverlay />
         <div class="icon-wrapper" v-html="$feather['thumbs-up'].toSvg({width:33,height:33})"></div>
         <div class="info">
           <div>Likes</div>
           <strong class="text-sm">1,213</strong>
-          <Progress percentage="70" bgClass="mt-2 mb-1 bg-white bg-opacity-20" fgClass="bg-slate-700"/>
+          <Progress percentage="70" bgClass="mt-2 mb-1 bg-white bg-opacity-20" fgClass="bg-slate-700" />
           <small>70% increase in 30 days</small>
         </div>
       </div>
     </div>
 
-    <div><h5>INFO BOX 3</h5></div>
+    <div>
+      <h5>INFO BOX 3</h5>
+    </div>
     <div class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-4 mb-2">
       <div class="info-box3 primary">
         <div class="wrapper">
@@ -201,7 +209,9 @@
             <div class="text-3xl font-bold mb-2">1,430</div>
             <span>Messages</span>
           </div>
-          <div class="icon-wrapper" v-html="$feather['message-square'].toSvg({'stroke-width':1.4,class:'text-white',fill:'rgba(0,0,0,.6)',width:70,height:70})"></div>
+          <div class="icon-wrapper"
+            v-html="$feather['message-square'].toSvg({'stroke-width':1.4,class:'text-white',fill:'rgba(0,0,0,.6)',width:70,height:70})">
+          </div>
         </div>
         <NuxtLink class="link" to="/">More Info</NuxtLink>
       </div>
@@ -211,7 +221,9 @@
             <div class="text-3xl font-bold mb-2">1,430</div>
             <span>Alert</span>
           </div>
-          <div class="icon-wrapper" v-html="$feather['alert-triangle'].toSvg({'stroke-width':1.4,class:'text-black',fill:'rgba(0,0,0,.6)',width:70,height:70})"></div>
+          <div class="icon-wrapper"
+            v-html="$feather['alert-triangle'].toSvg({'stroke-width':1.4,class:'text-black',fill:'rgba(0,0,0,.6)',width:70,height:70})">
+          </div>
         </div>
         <NuxtLink class="link" to="/">More Info</NuxtLink>
       </div>
@@ -221,7 +233,9 @@
             <div class="text-3xl font-bold mb-2">10</div>
             <span>Bookmark</span>
           </div>
-          <div class="icon-wrapper" v-html="$feather['bookmark'].toSvg({'stroke-width':1.4,class:'text-black',fill:'rgba(0,0,0,.6)',width:70,height:70})"></div>
+          <div class="icon-wrapper"
+            v-html="$feather['bookmark'].toSvg({'stroke-width':1.4,class:'text-black',fill:'rgba(0,0,0,.6)',width:70,height:70})">
+          </div>
         </div>
         <NuxtLink class="link text-gray-800" to="/">More Info</NuxtLink>
       </div>
@@ -231,7 +245,9 @@
             <div class="text-3xl font-bold mb-2">1,213</div>
             <span>Likes</span>
           </div>
-          <div class="icon-wrapper" v-html="$feather['thumbs-up'].toSvg({'stroke-width':1.4,class:'text-black',fill:'rgba(0,0,0,.6)',width:70,height:70})"></div>
+          <div class="icon-wrapper"
+            v-html="$feather['thumbs-up'].toSvg({'stroke-width':1.4,class:'text-black',fill:'rgba(0,0,0,.6)',width:70,height:70})">
+          </div>
         </div>
         <NuxtLink class="link" to="/">More Info</NuxtLink>
       </div>
@@ -241,7 +257,9 @@
             <div class="text-3xl font-bold mb-2">1,213</div>
             <span>Likes</span>
           </div>
-          <div class="icon-wrapper" v-html="$feather['thumbs-up'].toSvg({'stroke-width':1.4,class:'text-black',fill:'rgba(0,0,0,.6)',width:70,height:70})"></div>
+          <div class="icon-wrapper"
+            v-html="$feather['thumbs-up'].toSvg({'stroke-width':1.4,class:'text-black',fill:'rgba(0,0,0,.6)',width:70,height:70})">
+          </div>
         </div>
         <NuxtLink class="link" to="/">More Info</NuxtLink>
       </div>
@@ -251,24 +269,30 @@
             <div class="text-3xl font-bold mb-2">1,213</div>
             <span>Likes</span>
           </div>
-          <div class="icon-wrapper" v-html="$feather['thumbs-up'].toSvg({'stroke-width':1.4,class:'text-black',fill:'rgba(0,0,0,.6)',width:70,height:70})"></div>
+          <div class="icon-wrapper"
+            v-html="$feather['thumbs-up'].toSvg({'stroke-width':1.4,class:'text-black',fill:'rgba(0,0,0,.6)',width:70,height:70})">
+          </div>
         </div>
         <NuxtLink class="link" to="/">More Info</NuxtLink>
       </div>
       <div class="info-box3 info">
-        <LoadingOverlay/>
+        <LoadingOverlay />
         <div class="wrapper">
           <div class="info">
             <div class="text-3xl font-bold mb-2">1,213</div>
             <span>Likes</span>
           </div>
-          <div class="icon-wrapper" v-html="$feather['thumbs-up'].toSvg({'stroke-width':1.4,class:'text-black',fill:'rgba(0,0,0,.6)',width:70,height:70})"></div>
+          <div class="icon-wrapper"
+            v-html="$feather['thumbs-up'].toSvg({'stroke-width':1.4,class:'text-black',fill:'rgba(0,0,0,.6)',width:70,height:70})">
+          </div>
         </div>
         <NuxtLink class="link" to="/">More Info</NuxtLink>
       </div>
     </div>
 
-    <div><h5>ALERT</h5></div>
+    <div>
+      <h5>ALERT</h5>
+    </div>
     <div>
       <div class="alert primary soft mb-2">
         <div class="alert-content">
@@ -344,7 +368,10 @@
         </div>
       </div>
 
+
+
       <div class="alert primary mb-2">
+        <div class="icon" v-html="$feather.check.toSvg()"></div>
         <div class="alert-content">
           <strong class="title">Primary alert</strong>
           <p class="description"> Thank you for joining our newsletter </p>
@@ -383,6 +410,7 @@
 
 
       <div class="alert primary bordered mb-2">
+        <div class="icon" v-html="$feather.check.toSvg()"></div>
         <div class="alert-content">
           <strong class="title">Primary alert</strong>
           <p class="description"> Thank you for joining our newsletter </p>
@@ -423,6 +451,7 @@
 
 
       <div class="alert primary soft mb-2">
+        <div class="icon" v-html="$feather.check.toSvg()"></div>
         <div class="alert-content">
           <strong class="title">Primary alert</strong>
           <p class="description"> Thank you for joining our newsletter </p>
@@ -430,6 +459,7 @@
         <button class="btn-close" v-html="$feather.x.toSvg()"></button>
       </div>
       <div class="alert secondary soft mb-2">
+        <div class="icon" v-html="$feather.check.toSvg()"></div>
         <div class="alert-content">
           <strong class="title">Secondary alert</strong>
           <p class="description"> Thank you for joining our newsletter </p>
@@ -437,6 +467,7 @@
         <button class="btn-close" v-html="$feather.x.toSvg()"></button>
       </div>
       <div class="alert warning soft mb-2">
+        <div class="icon" v-html="$feather['alert-triangle'].toSvg()"></div>
         <div class="alert-content">
           <strong class="title">Warning alert</strong>
           <p class="description"> Thank you for joining our newsletter </p>
@@ -444,6 +475,7 @@
         <button class="btn-close" v-html="$feather.x.toSvg()"></button>
       </div>
       <div class="alert danger soft mb-2">
+        <div class="icon" v-html="$feather.x.toSvg()"></div>
         <div class="alert-content">
           <strong class="title">Danger alert</strong>
           <p class="description"> Thank you for joining our newsletter </p>
@@ -451,6 +483,7 @@
         <button class="btn-close" v-html="$feather.x.toSvg()"></button>
       </div>
       <div class="alert success soft mb-2">
+        <div class="icon" v-html="$feather.check.toSvg()"></div>
         <div class="alert-content">
           <strong class="title">Success alert</strong>
           <p class="description"> Thank you for joining our newsletter </p>
@@ -458,9 +491,56 @@
         <button class="btn-close" v-html="$feather.x.toSvg()"></button>
       </div>
       <div class="alert info soft mb-2">
+        <div class="icon" v-html="$feather.info.toSvg()"></div>
         <div class="alert-content">
           <strong class="title">Info alert</strong>
           <p class="description"> Thank you for joining our newsletter </p>
+        </div>
+        <button class="btn-close" v-html="$feather.x.toSvg()"></button>
+      </div>
+
+
+
+
+      <div class="alert primary soft mb-2">
+        <div class="icon" v-html="$feather.check.toSvg()"></div>
+        <div class="alert-content">
+          <strong class="title">Primary alert</strong>
+        </div>
+        <button class="btn-close" v-html="$feather.x.toSvg()"></button>
+      </div>
+      <div class="alert secondary soft mb-2">
+        <div class="icon" v-html="$feather.check.toSvg()"></div>
+        <div class="alert-content">
+          <strong class="title">Secondary alert</strong>
+        </div>
+        <button class="btn-close" v-html="$feather.x.toSvg()"></button>
+      </div>
+      <div class="alert warning soft mb-2">
+        <div class="icon" v-html="$feather['alert-triangle'].toSvg()"></div>
+        <div class="alert-content">
+          <strong class="title">Warning alert</strong>
+        </div>
+        <button class="btn-close" v-html="$feather.x.toSvg()"></button>
+      </div>
+      <div class="alert danger soft mb-2">
+        <div class="icon" v-html="$feather.x.toSvg()"></div>
+        <div class="alert-content">
+          <strong class="title">Danger alert</strong>
+        </div>
+        <button class="btn-close" v-html="$feather.x.toSvg()"></button>
+      </div>
+      <div class="alert success soft mb-2">
+        <div class="icon" v-html="$feather.check.toSvg()"></div>
+        <div class="alert-content">
+          <strong class="title">Success alert</strong>
+        </div>
+        <button class="btn-close" v-html="$feather.x.toSvg()"></button>
+      </div>
+      <div class="alert info soft mb-2">
+        <div class="icon" v-html="$feather.info.toSvg()"></div>
+        <div class="alert-content">
+          <strong class="title">Info alert</strong>
         </div>
         <button class="btn-close" v-html="$feather.x.toSvg()"></button>
       </div>
